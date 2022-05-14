db7f3ecc469f0744c6884758f50fc9f9b057a7ff
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2022-05-14 14:35:33 +0300

    rgdd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle'
 const iframe = document.querySelector('iframe')
-const player = new VideoPlaybackQuality.Player(iframe)
+const player = new Vimeo.Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000))
 function onPlay({ sec }) {
   localStorage.setItem('video-player-current-time', sec)
