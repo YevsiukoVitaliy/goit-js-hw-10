9fea83f294ac52674f5e708178eb58b67a13c25f
YevsiukoVitaliy <93053290+YevsiukoVitaliy@users.noreply.github.com>
2022-05-14 14:44:51 +0300

    hgngh

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,8 @@ import throttle from 'lodash.throttle'
 const iframe = document.querySelector('iframe')
 const player = new Vimeo.Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000))
-function onPlay({ sec }) {
-  localStorage.setItem('video-player-current-time', sec)
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds)
 }
-player.setCurrentTime(localStorage.getItem('video-player-current-time'))
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+
